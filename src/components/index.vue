49c3e477b665b08537fa80e3ce2730413c6b587f
stevenMty <990418195@qq.com>
2018-09-06 17:22:34 +0800

    first commit

---
+++ src/components/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <!-- <router-link to="/index/my">测试一下</router-link> -->
+    <button @click="jump">测试</button>
+    <button @click="jumpT">测试同级</button>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'xxx'
+    }
+  },
+  methods: {
+    jump () {
+      this.$router.push({ path: '/index/my' })
+    },
+    jumpT () {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/my-detial.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>xxx</h1>
+    <div>
+      这里是内容啊
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myDetial',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  color: red;
+}
+</style>
